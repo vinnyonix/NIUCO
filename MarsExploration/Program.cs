3afad973692dc72d2c0fd0e7742d6007ece98185
vinnyonix <vinnyonix@gmail.com>
2025-06-16 10:49:41 -0300

    Remoção de espaços.

--- MarsExploration/Program.cs
+++ MarsExploration/Program.cs
@@ -5,8 +5,6 @@ Console.WriteLine("Hello, World!");
 
 var plateau = new Plateau(5, 5);
 var rover = new Rover(new Position(3, 2), Direction.N, plateau);
-
 rover.ExecuteCommands("MRM");
-
 Console.WriteLine($"{rover.Position.X} {rover.Position.Y} {rover.Direction}");
 
