371b58120d33c263eaa2d53f48103d2d8b930f9f
vinnyonix <vinnyonix@gmail.com>
2025-06-16 00:02:30 -0300

    Adicionado o model Position

---
+++ MarsExploration/Domain/Models/Position.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsExploration.Domain.Models
+{
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position Clone() => new Position(X, Y);
+    }
+}
