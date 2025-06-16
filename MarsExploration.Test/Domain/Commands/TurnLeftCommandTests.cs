80a98b0b191aceb1210f83a548c476fd3d303f50
vinnyonix <vinnyonix@gmail.com>
2025-06-16 09:28:28 -0300

    Criado a TurnLeftCommandTests.

---
+++ MarsExploration.Test/Domain/Commands/TurnLeftCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using MarsExploration.Domain.Commands;
+using MarsExploration.Domain.Enums;
+using MarsExploration.Domain.Models;
+
+namespace MarsExploration.Test.Domain.Commands
+{
+    public class TurnLeftCommandTests
+    {
+        [Fact]
+        public void Execute_ShouldTurnRoverLeft()
+        {
+            var rover = new Rover(new Position(0, 0), Direction.N, new Plateau(5, 5));
+            var command = new TurnLeftCommand();
+
+            command.Execute(rover);
+
+            Assert.Equal(Direction.W, rover.Direction);
+        }
+    }
+}
+
