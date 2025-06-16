ab41c65d9f2a30f7de756b37a55f64093875d867
vinnyonix <vinnyonix@gmail.com>
2025-06-16 00:06:11 -0300

    Adicionado o command TurnRight.

---
+++ MarsExploration/Domain/Commands/TurnRightCommand.cs
@@ -0,0 +1,10 @@
+﻿using MarsExploration.Domain.Interfaces;
+using MarsExploration.Domain.Models;
+
+namespace MarsExploration.Domain.Commands
+{
+    public class TurnRightCommand : IHoverMovementCommand
+    {
+        public void Execute(Rover rover) => rover.TurnRight();
+    }
+}
