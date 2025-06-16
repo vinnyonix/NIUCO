2f6718f7ab53d10b6e1704c6b637e43b07027db4
vinnyonix <vinnyonix@gmail.com>
2025-06-16 09:26:14 -0300

    Criado a PositionTests.

---
+++ MarsExploration.Test/Domain/Models/PositionTests.cs
@@ -0,0 +1,16 @@
+﻿using MarsExploration.Domain.Models;
+
+namespace MarsExploration.Test.Domain.Models
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void Constructor_ShouldSetXAndY()
+        {
+            var position = new Position(3, 4);
+
+            Assert.Equal(3, position.X);
+            Assert.Equal(4, position.Y);
+        }
+    }
+}
